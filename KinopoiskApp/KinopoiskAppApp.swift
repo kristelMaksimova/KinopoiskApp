d26efbd505b21978da835a93a8728360bf2b99d9
Kristel <idmaksimovak@gmail.com>
2023-04-24 17:19:38 +0300

    Initial Commit

---
+++ KinopoiskApp/KinopoiskAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  KinopoiskAppApp.swift
+//  KinopoiskApp
+//
+//  Created by Kristi on 24.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct KinopoiskAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
